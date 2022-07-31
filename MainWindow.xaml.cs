e09cb67954b978bd5dc3488cec198f82f9b2e18d
Dustin Seger <3589164+jexorDev@users.noreply.github.com>
2022-07-31 10:06:22 -0500

    init

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TickTacToe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new TickTacToeViewModel();
+            btn_0_0.Click += btnClicked;
+            btn_0_1.Click += btnClicked;
+            btn_0_2.Click += btnClicked;
+            btn_1_0.Click += btnClicked;
+            btn_1_1.Click += btnClicked;
+            btn_1_2.Click += btnClicked;
+            btn_2_0.Click += btnClicked;
+            btn_2_1.Click += btnClicked;
+            btn_2_2.Click += btnClicked;
+
+            (DataContext as TickTacToeViewModel).ResetBoard();
+        }
+
+        public void ResetButtonClick(object sender, EventArgs e)
+        {
+            (DataContext as TickTacToeViewModel).ResetBoard();
+
+        }
+
+        private void btnClicked(object sender, EventArgs e)
+        {
+            if (sender == null) return;
+            if (sender is not Button) return;
+            Button? btnObject = (sender as Button);
+
+            var nameArray = btnObject.Name.Split("_");
+            int.TryParse(nameArray[1].ToString(), out int x);
+            int.TryParse(nameArray[2].ToString(), out int y);
+
+            (DataContext as TickTacToeViewModel).PerformPlayerTurn(x, y);
+        }
+    }
+}
---
+++ TickTacToeViewModel.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TickTacToe
+{
+    public class TickTacToeViewModel : INotifyPropertyChanged
+    {
+        private char[,] _board = new char[3, 3];
+        private char _playerSymbol = 'X';
+        private char _computerSymbol = 'O';
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+
+        public void PerformPlayerTurn(int x, int y)
+        {
+            
+
+            _board[x, y] = _playerSymbol;
+            NotifyPropertyChanged($"Button{x}{y}Text");
+
+
+            var playerWon = HasWinningRow(_playerSymbol);
+
+            if (playerWon)
+            {
+                MainLabelText = "You won!";
+                NotifyPropertyChanged(nameof(MainLabelText));
+
+                return;
+            }
+
+           
+            
+            if (!HasSpotAvailable())
+            {
+                MainLabelText = "Cat's game";
+                NotifyPropertyChanged(nameof(MainLabelText));
+
+                return;
+            }
+
+            PerformComputersTurn();
+
+            var computerWon = HasWinningRow(_playerSymbol);
+
+            if (computerWon)
+            {
+                MainLabelText = "Computer won :[";
+                NotifyPropertyChanged(nameof(MainLabelText));
+
+                return;
+            }
+
+        }
+
+        private void PerformComputersTurn()
+        {
+            var random = new Random();
+
+            while (true)
+            {
+                var nextX = random.Next(3);
+                var nextY = random.Next(3);
+                if (_board[nextX, nextY] == ' ')
+                {
+                    _board[nextX, nextY] = _computerSymbol;
+                    NotifyPropertyChanged($"Button{nextX}{nextY}Text");
+                    return;
+                }
+            }
+        }
+
+        private bool HasSpotAvailable()
+        {
+            for (int row = 0; row < 3; row++)
+            {
+                for (int column = 0; column < 3; column++)
+                {
+                    if (_board[row, column] == ' ')
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private bool HasWinningRow(char symbol)
+        {
+            for (int row = 0; row < 3; row++)
+            {
+                if (_board[row, 0] == symbol && _board[row, 1] == symbol && _board[row, 2] == symbol)
+                {
+                    return true;
+                }
+            }
+            for (int column = 0; column < 3; column++)
+            {
+                if (_board[0, column] == symbol && _board[1, column] == symbol && _board[2, column] == symbol)
+                {
+                    return true;
+                }
+            }
+            if (_board[0, 0] == symbol && _board[1, 1] == symbol && _board[2, 2] == symbol)
+            {
+                return true;
+            }
+            if (_board[0, 2] == symbol && _board[1, 1] == symbol && _board[2, 0] == symbol)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void ResetBoard()
+        {
+            for (int row = 0; row < 3; row++)
+            {
+                for (int column = 0; column < 3; column++)
+                {
+                    _board[row, column] = ' ';
+                }
+            }
+            MainLabelText = $"You are {_playerSymbol}";
+        }
+
+        public string MainLabelText
+        {
+            get; private set;
+        }
+
+        #region buttons
+        private string GetButtonText(int x, int y)
+        {
+            return _board[x, y].ToString();
+        }
+
+        public string Button00Text
+        {
+            get { return GetButtonText(0, 0); }
+        }
+
+
+        public string Button01Text
+        {
+            get { return GetButtonText(0, 1); }
+        }
+
+        public string Button02Text
+        {
+            get { return GetButtonText(0, 2); }
+        }
+
+        public string Button10Text
+        {
+            get { return GetButtonText(1, 0); }
+        }
+
+        public string Button11Text
+        {
+            get { return GetButtonText(1, 1); }
+        }
+
+        public string Button12Text
+        {
+            get { return GetButtonText(1, 2); }
+        }
+
+        public string Button20Text
+        {
+            get { return GetButtonText(2, 0); }
+        }
+
+        public string Button21Text
+        {
+            get { return GetButtonText(2, 1); }
+        }
+
+        public string Button22Text
+        {
+            get { return GetButtonText(2, 2); }
+        }
+
+        #endregion
+
+    }
+}
